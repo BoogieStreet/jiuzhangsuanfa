d9034149e3b900745d5bb7968b1c27ce00fa8501
BoogieStreet <15652534101@163.com>
2018-10-05 00:44:20 -0400

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -39,10 +39,9 @@ public class Solution {
 Q: 为什么要用 start + 1 < end？而不是 start < end 或者 start <= end？
 
 A: 为了避免死循环。二分法的模板中，整个程序架构分为两个部分：
-
-通过 while 循环，将区间范围从 n 缩小到 2 （只有 start 和 end 两个点）。
-在 start 和 end 中判断是否有解。
-start < end 或者 start <= end 在寻找目标最后一次出现的位置的时候，出现死循环。
+	1. 通过 while 循环，将区间范围从 n 缩小到 2 （只有 start 和 end 两个点；
+	2. 在 start 和 end 中判断是否有解。
+           start < end 或者 start <= end 在寻找目标最后一次出现的位置的时候，出现死循环。
 
 Q: 为什么明明可以 start = mid + 1 偏偏要写成 start = mid?
 
@@ -50,4 +49,5 @@ A: 大部分时候，mid 是可以 +1 和 -1 的。
 在一些特殊情况下，比如寻找目标的最后一次出现的位置时，当 target 与 nums[mid] 相等的时候，是不能够使用 mid + 1 或者 mid - 1 的。
 因为会导致漏掉解。那么为了节省脑力，统一写成 start = mid / end = mid 并不会造成任何解的丢失
 并且也不会损失效率——log(n) 和 log(n+1) 没有区别。
+
 */
