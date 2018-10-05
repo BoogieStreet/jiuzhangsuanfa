672872b601cd86306d9c95003a1729971f4b2789
BoogieStreet <15652534101@163.com>
2018-10-05 00:39:26 -0400

    二分法模板及解释

---
+++ binarySearch.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    public int findPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        // 要点1: start + 1 < end
+        while (start + 1 < end) {
+	    // 要点2：start + (end - start) / 2
+            int mid = start + (end - start) / 2;
+            // 要点3：=, <, > 分开讨论，mid 不+1也不-1
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        // 要点4: 循环结束后，单独处理start和end
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+}
