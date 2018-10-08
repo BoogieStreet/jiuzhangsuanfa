6cc18b7309e06f00edad94440365037f9e577bf0
BoogieStreet <15652534101@163.com>
2018-10-08 00:41:20 -0400

    Add files via upload

---
+++ mergeSort.java
@@ -0,0 +1,40 @@
+public class Solution {
+   
+    public void sortIntegers2(int[] A) {
+       
+        if(A==null||A.length==0){
+            return ;
+        }
+        int [] temp=new int[A.length];
+        mergeSort(A,0,A.length-1,temp);
+    }
+    public void mergeSort(int []nums,int start,int end,int[] temp){
+        if(start>=end) return;
+        mergeSort(nums,start,(start+end)/2,temp);
+        mergeSort(nums,(start+end)/2 + 1,end,temp);
+        merge(nums,start,end,temp);
+    }
+    public void merge(int []nums,int start,int end,int[]temp){
+        int mid=(start+end)/2;
+        int left=start;
+        int right=mid+1;
+        int i=left;
+        while(left<=mid&&right<=end){
+            if(nums[left]<nums[right]){
+                temp[i++]=nums[left++];
+            }
+            else{
+                temp[i++]=nums[right++];
+            }
+        }
+        while(left<=mid){
+            temp[i++]=nums[left++];
+        }
+        while(right<=end){
+            temp[i++]=nums[right++];
+        }
+        for(int j=start;j<=end;j++){
+            nums[j]=temp[j];
+        }
+    }
+}
