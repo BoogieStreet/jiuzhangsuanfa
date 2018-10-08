3029a513c0658f14888679d24f7f300eae4e2e0d
BoogieStreet <15652534101@163.com>
2018-10-08 14:40:51 -0400

    Add files via upload

---
+++ quickSelect.java
@@ -0,0 +1,39 @@
+public class Solution {
+   
+    public int kthLargestElement(int n, int[] nums) {
+
+        //k th largest means nums.length-k smallest(start from 0);
+        if(nums==null||nums.length==0||n<1||n>nums.length) return -1;
+        return quickSelect(nums,0,nums.length-1,nums.length-n);
+    }
+    public int quickSelect(int[]nums,int start,int end,int n){
+        if(start>=end){
+            return nums[n];
+        }
+        int pivot=nums[(start+end)/2];
+        int left=start;
+        int right=end;
+        while(left<=right){
+            while(left<=right&&nums[left]<pivot){
+                left++;
+            }
+            while(left<=right&&nums[right]>pivot){
+                right--;
+            }
+            if(left<=right){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
+            }
+        }
+        if(n<=nums[right]){
+            return quickSelect(nums,start,right,n);
+        }
+        if(n>=nums[left]){
+            return quickSelect(nums,left,end,n);
+        }
+        return nums[n];
+    }
+}
